5d7dd7e941871ea263897a1bf89367bdaa3efab9
skateboard <withdraws@gmail.com>
2024-10-11 13:05:38 +0400

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/data-harvesters/goapify-scraper
+
+go 1.22.1
+
+require github.com/data-harvesters/goapify v0.0.0-20241011053248-e4daefb664a0
---
+++ scraper.go
@@ -0,0 +1,73 @@
+package goapifyscraper
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/data-harvesters/goapify"
+)
+
+type State int
+
+const (
+	Initialize State = -1
+)
+
+type Scraper interface {
+	Next(state State) (State, error)
+
+	Stop()
+
+	Context() context.Context
+}
+
+type Base struct {
+	actor *goapify.Actor
+
+	context context.Context
+	cancel  func()
+}
+
+func New(actor *goapify.Actor) *Base {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	return &Base{
+		actor:   actor,
+		context: ctx,
+		cancel:  cancel,
+	}
+}
+
+func (b *Base) Stop() {
+	b.cancel()
+}
+
+func (b *Base) Context() context.Context {
+	return b.context
+}
+
+func Run(s Scraper) error {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	state := Initialize
+	var err error
+
+	for {
+		select {
+		default:
+			state, err = s.Next(state)
+			if err != nil {
+				return nil
+			}
+		case <-s.Context().Done():
+			return nil
+		}
+
+		time.Sleep(100 * time.Millisecond)
+	}
+}
